b243e928f389b54ab9ce065907d09961e018cc24
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2024-09-18 13:50:26 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,22 @@
-"use client"
-
+"use client";
 import * as React from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
-// import { ApolloProvider } from '@apollo/client';
-// import client from '@/lib/apolloClient';
+import { ApolloProvider } from '@apollo/client';
+// import client from '@/lib/apollo-client';
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <title>Phsycologist Feedback</title>
+        <title>Psychologist Feedback</title>
       </head>
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
             {/* <ApolloProvider client={client}> */}
               {props.children}
